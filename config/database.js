784b9ca6b9f4f6eddd822ac795b44e9b06b65c92
vali2244 <syedvali221@gmail.com>
2023-08-07 14:00:14 +0530

    production push

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose  = require('mongoose')
-// const DB=   "mongodb+srv://syedvali221:Babavali221@cluster0.wtcgclm.mongodb.net/?retryWrites=true&w=majority"
-const DB ="mongodb://127.0.0.1:27017/twitter"
+const DB=   "mongodb+srv://syedvali221:Babavali221@cluster0.wtcgclm.mongodb.net/?retryWrites=true&w=majority"
+// const DB ="mongodb://127.0.0.1:27017/twitter"
 
 exports.connect_mongo=()=>{
     mongoose.connect(DB)
